a5723d9be76a472d7e1fa87641309bf792a0db01
Dirck Hecking <dirck@appeligo.com>
2019-04-27 20:58:42 -0400

    logging in !!!

--- docs/js/main.js
+++ docs/js/main.js
@@ -2,19 +2,16 @@
 
 function login() {
 
-    const Url='https://allowme.appeligo.com/api/v1/login';
-    const Data='{"user":"dhecking@gmail.com","password":"ch@PT3r4"}';
-    const Config={
-        headers:{
-            "Content-type": "application/json"
-        },
-        body:Data,
-        method:"POST"
+    var url = 'https://allowme.appeligo.com/api/v1/login';
+    var init = {
+        headers: {"Content-type": "application/json"},
+        body: '{"user":"dhecking@gmail.com","password":"ch@PT3r4"}',
+        method: "POST"
     };
-    fetch(Url, Config)
+    fetch(url, init)
         .then(data => { return data.json() })
         .then(res => { console.log(res) })
-        .catch(error => console.log(error));
+        .catch(error => console.error(error));
 }
 
 login();
